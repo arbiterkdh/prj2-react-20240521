576e986ff5f897bed3f44e7c2d53c1d0b16ceb03
arbiterkdh <arbiterkdh@gmail.com>
2024-05-21 16:17:00 +0900

    수정 요청

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,17 @@ import { Home } from "./page/Home.jsx";
 import { BoardWrite } from "./page/board/BoardWrite.jsx";
 import { BoardList } from "./page/board/BoardList.jsx";
 import { BoardView } from "./page/board/BoardView.jsx";
+import { BoardEdit } from "./page/board/BoardEdit.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
     children: [
-      {
-        index: true,
-        element: <BoardList />,
-      },
-      {
-        path: "write",
-        element: <BoardWrite />,
-      },
-      {
-        path: "board/:id",
-        element: <BoardView />,
-      },
+      { index: true, element: <BoardList /> },
+      { path: "write", element: <BoardWrite /> },
+      { path: "board/:id", element: <BoardView /> },
+      { path: "edit/:id", element: <BoardEdit /> },
     ],
   },
 ]);
