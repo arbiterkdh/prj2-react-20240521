5d8a69bf77b04110d086a1dd5da94ec664b3a30a
arbiterkdh <arbiterkdh@gmail.com>
2024-05-22 11:15:35 +0900

    암호 확인 인풋 추가

--- src/page/member/MemberSignup.jsx
+++ src/page/member/MemberSignup.jsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   FormControl,
+  FormHelperText,
   FormLabel,
   Input,
   InputGroup,
@@ -15,6 +16,7 @@ import { useNavigate } from "react-router-dom";
 export function MemberSignup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
   const [nickName, setNickName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const toast = useToast();
@@ -99,6 +101,8 @@ export function MemberSignup() {
       .finally();
   }
 
+  const isCheckedPassword = password === passwordCheck;
+
   return (
     <Box>
       <Box>회원 가입</Box>
@@ -126,6 +130,18 @@ export function MemberSignup() {
               type={"password"}
               onChange={(e) => setPassword(e.target.value)}
             />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>암호확인</FormLabel>
+            <Input
+              type={"password"}
+              onChange={(e) => setPasswordCheck(e.target.value)}
+            />
+            {isCheckedPassword || (
+              <FormHelperText>암호가 일치하지 않습니다.</FormHelperText>
+            )}
           </FormControl>
         </Box>
         <Box>
