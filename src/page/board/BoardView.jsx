468334ad6da4cb409299d10a7ef4bfd50d80d3cd
arbiterkdh <arbiterkdh@gmail.com>
2024-05-29 14:54:37 +0900

    좋아요 하트 출력

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -4,8 +4,10 @@ import axios from "axios";
 import {
   Box,
   Button,
+  Flex,
   FormControl,
   FormLabel,
+  Heading,
   Image,
   Input,
   Modal,
@@ -20,12 +22,17 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import { LoginContext } from "../../component/LoginProvider.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp as fullyThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import { faThumbsUp as emptyThumbsUp } from "@fortawesome/free-regular-svg-icons/faThumbsUp";
 
 export function BoardView() {
   const account = useContext(LoginContext);
 
   const { id } = useParams();
   const [board, setBoard] = useState(null);
+  const [like, setLike] = useState(false);
+
   const toast = useToast();
   const navigate = useNavigate();
   const { isOpen, onClose, onOpen } = useDisclosure();
@@ -77,9 +84,20 @@ export function BoardView() {
 
   return (
     <Box>
-      <Box sx={{ padding: "10px", fontSize: "1.5rem" }}>
-        {board.id}번 게시물
-      </Box>
+      <Flex>
+        <Heading sx={{ padding: "10px", fontSize: "1.5rem" }}>
+          {board.id}번 게시물
+        </Heading>
+        <Box
+          color={"blue.400"}
+          onClick={() => setLike(!like)}
+          fontSize={"3xl"}
+          cursor={"pointer"}
+        >
+          {like && <FontAwesomeIcon icon={fullyThumbsUp} />}
+          {like || <FontAwesomeIcon icon={emptyThumbsUp} />}
+        </Box>
+      </Flex>
       <Box>
         <FormControl>
           <FormLabel>제목</FormLabel>
