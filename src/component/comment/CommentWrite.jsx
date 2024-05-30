9b2b04b04b98f198de1986df59be2a2df12c786a
arbiterkdh <arbiterkdh@gmail.com>
2024-05-30 11:01:10 +0900

    댓글 작성

--- src/component/comment/CommentWrite.jsx
+++ src/component/comment/CommentWrite.jsx
@@ -2,11 +2,12 @@ import { Box, Button, Textarea } from "@chakra-ui/react";
 import { useState } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane as solidPlane } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane as emptyPlane } from "@fortawesome/free-regular-svg-icons";
 
 export function CommentWrite({ boardId }) {
   const [comment, setComment] = useState("");
-
+  const [mouse, setMouse] = useState(0);
   function handleCommentSubmitClick() {
     axios
       .post("/api/comment/add", {
@@ -25,8 +26,13 @@ export function CommentWrite({ boardId }) {
         value={comment}
         onChange={(e) => setComment(e.target.value)}
       />
-      <Button onClick={handleCommentSubmitClick}>
-        <FontAwesomeIcon icon={faPaperPlane} />
+      <Button
+        onClick={handleCommentSubmitClick}
+        onMouseEnter={() => setMouse(1)}
+        onMouseLeave={() => setMouse(0)}
+      >
+        {mouse === 1 && <FontAwesomeIcon icon={solidPlane} size={"xl"} />}
+        {mouse === 0 && <FontAwesomeIcon icon={emptyPlane} size={"xl"} />}
       </Button>
     </Box>
   );
