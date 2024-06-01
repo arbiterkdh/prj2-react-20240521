868ab2807be091f2416f5acdc07f000201c6c355
arbiterkdh <arbiterkdh@gmail.com>
2024-06-01 15:07:34 +0900

    css 손보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { IconButton, useColorMode } from "@chakra-ui/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./page/Home.jsx";
 import { BoardWrite } from "./page/board/BoardWrite.jsx";
@@ -12,7 +11,6 @@ import { MemberInfo } from "./page/member/MemberInfo.jsx";
 import { MemberEdit } from "./page/member/MemberEdit.jsx";
 import { MemberLogin } from "./page/member/MemberLogin.jsx";
 import { LoginProvider } from "./component/LoginProvider.jsx";
-import { FaMoon, FaSun } from "react-icons/fa";
 import axios from "axios";
 
 // axios interceptor 설정
@@ -43,22 +41,11 @@ const router = createBrowserRouter([
 ]);
 
 function App(props) {
-  const { toggleColorMode, colorMode } = useColorMode();
   return (
     <>
       <LoginProvider>
         <RouterProvider router={router} />
       </LoginProvider>
-      <IconButton
-        aria-label="toggle theme"
-        rounded="full"
-        size="sm"
-        position="absolute"
-        bottom={4}
-        right={4}
-        onClick={toggleColorMode}
-        icon={colorMode === "dark" ? <FaSun /> : <FaMoon />}
-      />
     </>
   );
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,15 +1,29 @@
-import { Box } from "@chakra-ui/react";
+import { Box, IconButton, useColorMode } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import React from "react";
 import { Navbar } from "../component/Navbar.jsx";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 export function Home() {
+  const { toggleColorMode, colorMode } = useColorMode();
+
   return (
     <Box>
       <Navbar />
       <Box>
         <Outlet />
       </Box>
+
+      <IconButton
+        aria-label="toggle theme"
+        rounded="full"
+        size="md"
+        position={"fixed"}
+        bottom={2}
+        left={2}
+        onClick={toggleColorMode}
+        icon={colorMode === "dark" ? <FaSun /> : <FaMoon />}
+      />
     </Box>
   );
 }
